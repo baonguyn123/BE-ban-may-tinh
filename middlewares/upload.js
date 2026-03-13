f353e0c4492fca067aeeb58f2181d623eac4dddc
baonguyn123 <bonvipro123z@gmail.com>
2026-03-14 00:41:13 +0700

    push

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,7 +1,7 @@
 const multer = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../nodejs/public/images/');
+        cb(null, './public/images/');
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
